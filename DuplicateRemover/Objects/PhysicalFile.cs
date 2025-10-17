a070eaf02c8d7c9c2ef5b08b9e8d1229d4457b29
dmentizis <dmentizis@gmail.com>
2025-10-17 17:11:31 +0300

    Refactor file classes and clean up namespaces
    Removed unnecessary using directives from PhysicalFile.cs and UniqueFile.cs. Updated namespace declarations to follow standard C# syntax. Introduced PhysicalFile class with required Path and SizeInBytes properties, and added UniqueFile class with a constructor.

--- DuplicateRemover/Objects/PhysicalFile.cs
+++ DuplicateRemover/Objects/PhysicalFile.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DuplicateRemover.Objects
+﻿namespace DuplicateRemover.Objects
 {
     public class PhysicalFile
     {
